219e3e7d337576789c55ef93ae68e2c5d725903b
codehezi <2297142423@qq.com>
2022-07-22 23:23:28 +0800

    Site updated: 2022-07-22 23:23:28

--- js/diy/music.js
+++ js/diy/music.js
@@ -4,12 +4,6 @@ const ap = new APlayer ({
   autoplay:true,//自动播放
   audio:[
   {
-   name:"浴室",
-   artist:"deca jions",
-   url:"https://m801.music.126.net/20220722194213/2cea2614a3d0177d40137c2acf6e0cb1/jdyyaac/000c/560b/5153/7f43625368aa52c4fbb0f968fa2007d2.m4a",
-   cover:"https://p1.music.126.net/byjfkEIOWI_RmxSKEWTPyw==/18910500486297525.jpg?param=200y200",
-  },
-  {
   name:"杏花弦外雨",
   artist:"CRITTY/司夏",
   url:"https://m10.music.126.net/20220722200200/5cdb729870b8f332b7f8bc762d7c74b0/yyaac/obj/wonDkMOGw6XDiTHCmMOi/2976648142/11b2/373d/7ecd/f6199141769e8ce91ea527f77fbfc338.m4a",
